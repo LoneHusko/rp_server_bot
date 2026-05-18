b113bec20aa799d13cffe0c9c89fbce4427ee9ca
LoneHusko <90392521+LoneHusko@users.noreply.github.com>
2026-05-18 16:40:16 +0200

    basic setup and requirements

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+import discord
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+intents = discord.Intents.default()
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+BOT_TOKEN = os.getenv("TOKEN")
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN is not set")
+else:
+    client.run(BOT_TOKEN)
