fba672354c69a7647e6431cb967dd4668f60397b
LoneHusko <90392521+LoneHusko@users.noreply.github.com>
2026-05-18 18:10:03 +0200

    add GUILD_ID and DEFAULT_ROLE_ID to env variables and assign default role on member join

--- cogs/members.py
+++ cogs/members.py
@@ -1,5 +1,15 @@
+import os
 import discord
+import dotenv
 from discord.ext import commands
+
+dotenv.load_dotenv()
+GUILD_ID = int(os.getenv("GUILD_ID", 0))
+if GUILD_ID == 0:
+    raise RuntimeError("GUILD_ID is not set")
+DEFAULT_ROLE_ID = int(os.getenv("DEFAULT_ROLE_ID", 0))
+if DEFAULT_ROLE_ID == 0:
+    raise RuntimeError("DEFAULT_ROLE_ID is not set")
 
 
 class MembersCog(commands.Cog):
@@ -7,12 +17,30 @@ class MembersCog(commands.Cog):
     Handles all member-related events and commands
     """
 
-    def __init__(self, bot) -> None:
+    def __init__(self, bot: discord.ext.commands.Bot) -> None:
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member) -> None:
+        """
+        Event listener for when a member joins a Discord server.
+
+        This function listens for the `on_member_join` event, which is triggered when a member joins
+        the server. It assigns a default role to the newly joined member.
+
+        :param member: The `discord.Member` object representing the member who joined the server.
+        :type member: discord.Member
+        :raises RuntimeError: If the bot is not in the target guild or the default role cannot be found.
+        :return: None
+        """
         print(f"{member} has joined the server!")
+        guild = self.bot.get_guild(GUILD_ID)
+        if guild is None:
+            raise RuntimeError("Bot is not in the target guild")
+        role = guild.get_role(DEFAULT_ROLE_ID)
+        if role is None:
+            raise RuntimeError("Default role is not found on this guild")
+        await member.add_roles(role)
 
 
 async def setup(bot) -> None:
